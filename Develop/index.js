2f7c299feed7f1f718cc87602c03b5dbfba21dc9
Tori Acer <vrice360@gmail.com>
2022-04-03 23:17:51 -0500

    first commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+// const {writeFile}
+// const inquirer = require("")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// application repo info should ask: title of project, sections, entitled description,
+// Table of contents, Installation
+// usage, license (badges) , contributors, contribution guidelines, tests, and questions
+const questions = [
+return inquirer.prompt([
+
+  type
+])
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
